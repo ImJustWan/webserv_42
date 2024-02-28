ce278752189f9347dfc3df27c06fb417e67ade60
mrony <marie.rony@gmail.com>
2024-02-28 19:33:43 +0100

    Edited bash script so that it takes an argument and checks its existence

--- www/files_to_upload/chunking.sh
+++ www/files_to_upload/chunking.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-file="HAHAHA.png"
+# Check if a file is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <file>"
+    exit 1
+fi
+
+# Get the filename from the first argument
+file="$1"
+
+# Check if the file exists
+if [ ! -f "$file" ]; then
+    echo "File $file not found!"
+    exit 1
+fi
+
+# Check if the file is readable
+if [ ! -r "$file" ]; then
+    echo "File $file is not accessible!"
+    exit 1
+fi
+
+# Proceed with the script if the file exists and is accessible
+
 chunk_size=10000
 file_size=$(wc -c < "$file")
 
@@ -13,7 +35,5 @@ done
 
 # Print the final chunk indicating the end
 # printf "\r\n0\r\n\r\n"
-
-
 
 curl -X POST -H "Transfer-Encoding: chunked; filename=\"$file\"" --data-binary "@$file" localhost:8080
